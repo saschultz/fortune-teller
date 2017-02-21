055e0c46ff2ec7a9dff7b91720269c7a19ad7ba0
Sara Schultz and Shane Stafford <shane@>
2017-02-21 15:24:33 -0800

    add variables and functions for user responses in checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,20 @@
 var unluckyResult = (unluckyCheck.length > luckyCheck.length)
 var luckyResult = (luckyCheck.length > unluckyCheck.length)
 var mediocreResult = (luckyCheck.length === unluckyCheck.length)
+var unluckyCheck = [];
+var luckyCheck = [];
 
 //Front-end logic
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
+    $("input:checkbox[name=unlucky]:checked").each(function() {
+      var unlucky = $(this).val();
+      unluckyCheck.push(unlucky);
+      });
+    $("input:checkbox[name=lucky]:checked").each(function() {
+      var lucky = $(this).val();
+      luckyCheck.push(lucky);
+      });
   });
 });
