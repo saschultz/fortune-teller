7ec7517e32f3a79c1ea0b3922d48363e9a3c7b4e
Sara Schultz and Shane Stafford <sara@>
2017-02-21 15:15:05 -0800

    correct name for lucky inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
+//Back-end logic
+var unluckyResult = (unluckyCheck.length > luckyCheck.length)
+var luckyResult = (luckyCheck.length > unluckyCheck.length)
+var mediocreResult = (luckyCheck.length === unluckyCheck.length)
 
+//Front-end logic
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault();
+  });
 });
